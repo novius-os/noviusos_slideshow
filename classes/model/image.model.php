8cca297a85f8b0c1cd12696bca6c9d77cd7dc522
Gilles FELIX <felix@novius.com>
2012-12-06 15:26:46 +0100

    Bugfix : namespace Model_Page have changed

--- classes/model/image.model.php
+++ classes/model/image.model.php
@@ -20,7 +20,7 @@ class Model_Image extends Model
     protected static $_has_one = array(
         'page' => array(
             'key_from'          => 'slidimg_link_to_page_id',
-            'model_to'          => '\Nos\Model_Page',
+            'model_to'          => '\Nos\Page\Model_Page',
             'key_to'            => 'page_id',
             'cascade_save'      => false,
             'cascade_delete'    => false,
