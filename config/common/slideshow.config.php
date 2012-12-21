d8a95fa86252a7f7182ed686201bfc9c150a3d19
esperat <esperat@novius.fr>
2012-12-21 16:53:21 +0100

    "Updating CRUD translations"

--- config/common/slideshow.config.php
+++ config/common/slideshow.config.php
@@ -12,16 +12,22 @@ return array(
     ),
     'i18n' => array(
         // Crud
-        'successfully added' => __('Done! The slideshow has been added.'),
-        'successfully deleted' => __('The slideshow has been deleted.'),
+        'notification item added' => __('All good! Your new slideshow has been added.'),
+        'notification item deleted' => __('The slideshow has been deleted.'),
 
         // General errors
-        'item deleted' => __('This slideshow doesn’t exist any more. It has been deleted.'),
-        'not found' => __('We cannot find this slideshow.'),
+        'notification item does not exist anymore' => __('This slideshow doesn’t exist any more. It has been deleted.'),
+        'notification item not found' => __('We cannot find this slideshow. Are you sure it exists?'),
 
         // Deletion popup
-        'delete an item' => __('Deleting the news slideshow ‘{{title}}’'),
-        'you are about to delete, confim' => __('Last chance, there’s no undo. Do you really want to delete this slideshow?'),
+        'deleting item title' => __('Deleting the slideshow ‘{{title}}’'),
+        'deleting confirmation' => __('Last chance, there’s no undo. Do you really want to delete this slideshow?'),
+
+        # Delete action's labels
+        'deleting button 1 item' => __('Delete this slideshow'),
+
+        '1 item' => __('1 slideshow'),
+        'N items' => __('{{count}} slideshows'),
     ),
     'actions' => array(
         'Nos\Slideshow\Model_Slideshow.add' => array(
