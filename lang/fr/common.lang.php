d28effd5a114c495aefb9b40a1db4af0218aefd1
Julian Esperat <esperat@novius.fr>
2013-01-30 15:49:01 +0100

    "Updated translations"

--- lang/fr/common.lang.php
+++ lang/fr/common.lang.php
@@ -1,72 +1,100 @@
 <?php
 
-// Generated on 23/01/2013 11:25:53
+// Generated on 29/01/2013 19:08:29
 
-// 35 out of 35 messages are translated (100%).
-// 145 out of 145 words are translated (100%).
+// 44 out of 44 messages are translated (100%).
+// 165 out of 165 words are translated (100%).
 
 return array(
-    #: views/admin/_form_content.view.php:43
-    #: views/admin/_form_content.view.php:109
+    #: views/admin/_form_content.view.php:46
+    #: views/admin/_form_content.view.php:112
     'Image' => 'Image',
 
-    #: views/admin/_form_content.view.php:80
+    #: views/admin/_form_content.view.php:83
     'Add a slide' => 'Ajouter une diapo',
 
-    #: views/admin/_form_content.view.php:113
+    #: views/admin/_form_content.view.php:89
+    'Delete' => 'Supprimer',
+
+    #: views/admin/_form_content.view.php:116
+    'Pick an image' => 'Sélectionner une image',
+
+    #: views/admin/_form_content.view.php:117
+    'Pick another image' => 'Sélectionner une autre image',
+
+    #: views/admin/_form_content.view.php:118
+    'No image' => 'Pas d’image',
+
+    #: views/admin/_form_content.view.php:119
+    'This extension is not allowed.' => 'Cette extension n’est pas autorisée.',
+
+    #: views/admin/_form_content.view.php:123
     'Are you sure you want to delete this slide?' => 'Êtes-vous sûr(e) de vouloir supprimer cette diapo&nbsp;?',
 
-    #: views/admin/preview.php:13
-    'Slideshow ‘{{title}}’' => 'Diaporama «&nbsp;{{title}}&nbsp;»',
-
-    #: views/admin/preview.php:14
-    '(format: {{format}})' => '(format&nbsp;: {{format}})',
-
-    #: views/admin/warning_not_published.view.php:15
-    'Not published' => 'Non publié',
-
-    #: views/admin/warning_not_published.view.php:16
-    'To publish this slideshow, add it to a page, a blog post or any other text editor.' => 'Pour publier ce diaporama, ajoutez-le à une page, un billet de blog ou toutautre éditeur de texte.',
-
-    #: views/admin/warning_not_published.view.php:19
-    '<p>Most Novius OS text editors (also called WYSIWYG editors) feature this button {{preview}} in the toolbar.</p><p>Click it to access the list of applications you can enhance your text with.</p>' => '<p>La plupart des éditeurs de texte de Novius OS (aussi appelés éditeursWYSIWYG) ont ce bouton {{preview}} dans la barre d’outils.</p><p>Cliquezpour accéder à la liste des applications permettant d’enrichir voscontenus.</p>',
-
-    #: views/admin/warning_not_published.view.php:20
-    'Applications' => 'Applications',
-
-    #: views/admin/_form_image.php:12
+    #: views/admin/_form_image.view.php:15
     'Properties' => 'Propriétés',
 
-    #: views/admin/_form_image.php:19
+    #: views/admin/_form_image.view.php:22
     'Image:' => 'Image&nbsp;:',
 
-    #: views/admin/_form_image.php:37
+    #: views/admin/_form_image.view.php:40
     'Slide name:' => 'Nom de la diapo&nbsp;:',
 
-    #: views/admin/_form_image.php:39
+    #: views/admin/_form_image.view.php:42
     'Slide name' => 'Nom de la diapo',
 
-    #: views/admin/_form_image.php:44
+    #: views/admin/_form_image.view.php:47
     'Description:' => 'Description&nbsp;:',
 
-    #: views/admin/_form_image.php:53
+    #: views/admin/_form_image.view.php:56
     'Links to:' => 'Lien vers&nbsp;:',
 
-    #: views/admin/_form_image.php:58
+    #: views/admin/_form_image.view.php:61
     'Add a link' => 'Ajouter un lien',
 
-    #: views/admin/_form_image.php:85
+    #: views/admin/_form_image.view.php:88
     'Remove the link' => 'Retirer le lien',
 
-    #: config/controller/admin/slideshow.config.php:17
-    #: config/common/slideshow.config.php:66
+    #: views/admin/enhancer/popup.view.php:15
+    'Select a slideshow:' => 'Sélectionner un diaporama&nbsp;:',
+
+    #: views/admin/enhancer/popup.view.php:30
+    'Format:' => 'Format&nbsp;:',
+
+    #: views/admin/enhancer/preview.view.php:16
+    'Slideshow ‘{{title}}’' => 'Diaporama «&nbsp;{{title}}&nbsp;»',
+
+    #: views/admin/enhancer/preview.view.php:17
+    '(format: {{format}})' => '(format&nbsp;: {{format}})',
+
+    #: views/admin/warning_not_published.view.php:18
+    'Not published' => 'Non publié',
+
+    #: views/admin/warning_not_published.view.php:19
+    'To publish this slideshow, add it to a page, a blog post or any other text editor.' => 'Pour publier ce diaporama, ajoutez-le à une page, un billet de blog ou toutautre éditeur de texte.',
+
+    #: views/admin/warning_not_published.view.php:22
+    '<p>Most Novius OS text editors (also called WYSIWYG editors) feature this button {{preview}} in the toolbar.</p><p>Click it to access the list of applications you can enhance your text with.</p>' => '<p>La plupart des éditeurs de texte de Novius OS (aussi appelés éditeursWYSIWYG) ont ce bouton {{preview}} dans la barre d’outils.</p><p>Cliquezpour accéder à la liste des applications permettant d’enrichir voscontenus.</p>',
+
+    #: views/admin/warning_not_published.view.php:23
+    'Applications' => 'Applications',
+
+    #: config/slideshow.config.php:19
+    'Big' => 'Grand',
+
+    #: config/slideshow.config.php:25
+    'Small' => 'Petit',
+
+    #: config/controller/admin/slideshow.config.php:19
+    #: config/common/slideshow.config.php:67
     'Add a slideshow' => 'Ajouter un diaporama',
 
-    #: config/controller/admin/slideshow.config.php:44
+    #: config/controller/admin/slideshow.config.php:46
     #: config/common/slideshow.config.php:9
     'Title' => 'Titre',
 
-    #: config/controller/admin/slideshow.config.php:58
+    #. Note to translator: This is a submit button
+    #: config/controller/admin/slideshow.config.php:61
     'Save' => 'Enregistrer',
 
     #: config/controller/admin/appdesk.config.php:21
@@ -84,38 +112,39 @@ return array(
     #: config/controller/admin/appdesk.config.php:25
     'No slideshows' => 'Pas de diaporama',
 
-    #: config/controller/admin/appdesk.config.php:26
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:27
     'Show all slideshows' => 'Afficher tous les diaporamas',
 
     #: config/common/slideshow.config.php:24
     'Slides' => 'Diapos',
 
     #. Crud
-    #: config/common/slideshow.config.php:48
+    #: config/common/slideshow.config.php:49
     'Added! This is a nice new slideshow you have here.' => 'C’est ajouté. En voilà un beau diaporama.',
 
-    #: config/common/slideshow.config.php:49
+    #: config/common/slideshow.config.php:50
     'The slideshow has been deleted.' => 'Ce diaporama a été supprimé.',
 
     #. General errors
-    #: config/common/slideshow.config.php:52
+    #: config/common/slideshow.config.php:53
     'This slideshow doesn’t exist any more. It has been deleted.' => 'Ce diaporama n’existe plus. Il a été supprimé.',
 
-    #: config/common/slideshow.config.php:53
+    #: config/common/slideshow.config.php:54
     'We cannot find this slideshow.' => 'Nous n’arrivons pas à trouver ce diaporama. Êtes-vous sûr(e) qu’ilexiste&nbsp;?',
 
     #. Deletion popup
-    #: config/common/slideshow.config.php:56
+    #: config/common/slideshow.config.php:57
     'Deleting the slideshow ‘{{title}}’' => 'Supprimer le diaporama «&nbsp;{{title}}&nbsp;»',
 
     #. Delete action's labels
-    #: config/common/slideshow.config.php:59
+    #: config/common/slideshow.config.php:60
     'Yes, delete this slideshow' => 'Oui, supprimer ce diaporama',
 
-    #: config/common/slideshow.config.php:61
+    #: config/common/slideshow.config.php:62
     '1 slideshow' => '1 diaporama',
 
-    #: config/common/slideshow.config.php:62
+    #: config/common/slideshow.config.php:63
     '{{count}} slideshows' => '{{count}} diaporamas',
 
 );
