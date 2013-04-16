73eeb48a03ed1ab33ce0f19774197f140d036c54
Julian Esperat <esperat@novius.fr>
2013-04-16 19:18:01 +0200

    "Updating dictionaries"

---
+++ lang/ja/metadata.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+// Generated on 16/04/2013 19:15:50
+
+// 0 out of 2 messages are translated (0%).
+// 0 out of 2 words are translated (0%).
+
+return array(
+    'Slideshows' => '',
+
+    'Slideshow' => '',
+
+);
