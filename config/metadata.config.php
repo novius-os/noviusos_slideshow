2ba32b839a7475f36f14a7de1dab7d40d128e12d
Gilles FELIX <felix@novius.com>
2013-12-02 13:37:08 +0100

    "Version Chiba 2.3.1"

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -11,7 +11,7 @@
 return array(
     'name'      => 'Slideshows',
     'namespace' => 'Nos\Slideshow',
-    'version' => 'chiba.2.3',
+    'version' => 'chiba.2.3.1',
     'provider'  => array(
         'name'  => 'Novius OS',
     ),
