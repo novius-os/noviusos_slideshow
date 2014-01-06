c5d01b2c8e78f1b632f766d82008dc7edbf1d4c1
Gilles FELIX <felix@novius.com>
2014-01-06 15:11:29 +0100

    "Add spanish translation"

---
+++ lang/es/common.lang.php
@@ -0,0 +1,147 @@
+<?php
+
+// Generated on 06/01/2014 15:07:24
+
+// 40 out of 40 messages are translated (100%).
+// 223 out of 223 words are translated (100%).
+
+return array(
+    #: views/admin/_form_content.view.php:36
+    'Add a slide' => 'Añadir una diapositiva',
+
+    #: views/admin/_form_content.view.php:42
+    'Delete' => 'Eliminar',
+
+    #: views/admin/_form_content.view.php:59
+    'Are you sure you want to delete this slide?' => '¿Estás seguro/a de que quieres eliminar todas las diapositivas?',
+
+    #: views/admin/enhancer/preview.view.php:16
+    'Slideshow ‘{{title}}’' => 'Presentación «{{title}}»',
+
+    #: views/admin/enhancer/preview.view.php:17
+    '(format: {{format}})' => '(formato: {{format}})',
+
+    #: views/admin/enhancer/blank_slate.view.php:16
+    'How frustrating, you have no slideshow to insert. But let’s not worry, shall we? Here is how it works:' => 'Vaya...aún no hay ninguna presentación. No pasa nada, te explicamos como funciona:',
+
+    #: views/admin/enhancer/blank_slate.view.php:19
+    '<ul><li><a>Add your first slideshow</a> (a new tab will open).</li>
+<li>Once you’re done (it won’t take long), come back to this tab to insert your shiny new slideshow.</li></ul>' => '<ul><li><a>Añade tu primera presentación</a> (se abre una nueva pestaña).</li>
+<li>Cuando acabes (se tarda poco), vuelve a esta pestaña para insertar tu presentación recién salida del horno.</li></ul>',
+
+    #: views/admin/enhancer/blank_slate.view.php:27
+    'No slideshows are available in {{context}}. Go ahead, <a>add your first slideshow in this language</a>.' => 'No hay ninguna presentación en {{context}}. Anímate y <a>añade tu primera presentación en este idioma</a>.',
+
+    #: views/admin/enhancer/blank_slate.view.php:29
+    'No slideshows are available in {{context}}. Go ahead, <a>add your first slideshow in this context</a>.' => 'No hay ninguna presentación en {{context}}. Anímate y <a>añade tu primera presentación en este contexto</a>.',
+
+    #: views/admin/warning_not_published.view.php:18
+    'Not published' => 'Sin publicar',
+
+    #: views/admin/warning_not_published.view.php:19
+    'To publish this slideshow, add it to a page, a blog post or any other text editor.' => 'Para publicar esta presentación, añádela a una página, un artículo o cualquier otro editor de texto.',
+
+    #: views/admin/warning_not_published.view.php:22
+    '<p>Most Novius OS text editors (also called WYSIWYG editors) feature this button {{preview}} in the toolbar.</p><p>Click it to access the list of applications you can enhance your text with.</p>' => '<p>La mayoría de los editores de texto de Novius OS (también llamados editores WYSIWYG) incluyen este botón {{preview}} en la barra de herramientas.</p><p>Haz clic en él para acceder a la lista de aplicaciones con las que puedes mejorar el contenido.</p>',
+
+    #: views/admin/warning_not_published.view.php:23
+    'Applications' => 'Aplicaciones',
+
+    #: views/admin/renderer/page_selector.view.php:6
+    'Add a link' => 'Añadir un enlace',
+
+    #: views/admin/renderer/page_selector.view.php:10
+    'Remove the link' => 'Quitar el enlace',
+
+    #: config/slideshow.config.php:17
+    'Big Flexslider v1.8' => 'Flexslider v1.8 en grande',
+
+    #: config/slideshow.config.php:37
+    'Small Flexslider v1.8' => 'Flexslider v1.8 en pequeño',
+
+    #: config/controller/admin/slideshow.config.php:19
+    #: config/common/slideshow.config.php:79
+    'Add a slideshow' => 'Añadir una presentación',
+
+    #: config/controller/admin/slideshow.config.php:45
+    #: config/common/slideshow.config.php:9
+    'Title' => 'Título',
+
+    #: config/controller/admin/slideshow.config.php:68
+    'Image:' => 'Imagen:',
+
+    #: config/controller/admin/slideshow.config.php:76
+    'Image' => 'Imagen',
+
+    #: config/controller/admin/slideshow.config.php:92
+    'Slide name:' => 'Nombre de la diapositiva',
+
+    #: config/controller/admin/slideshow.config.php:95
+    'Description:' => 'Descripción:',
+
+    #: config/controller/admin/slideshow.config.php:105
+    'Links to:' => 'Enlace hacia:',
+
+    #: config/controller/admin/slideshow.config.php:119
+    'Properties' => 'Propiedades',
+
+    #: config/controller/admin/enhancer.config.php:22
+    'Select a slideshow:' => 'Seleccionar una presentación:',
+
+    #: config/controller/admin/enhancer.config.php:28
+    'Format:' => 'Formato:',
+
+    #: config/controller/admin/appdesk.config.php:21
+    'slideshow' => 'presentación',
+
+    #: config/controller/admin/appdesk.config.php:22
+    'slideshows' => 'presentaciones',
+
+    #: config/controller/admin/appdesk.config.php:24
+    #: config/common/slideshow.config.php:73
+    '1 slideshow' => array(
+        0 => '1 presentación',
+        1 => '{{count}} presentaciones',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:28
+    'Showing 1 slideshow out of {{y}}' => array(
+        0 => 'Se muestra 1 presentación de las {{y}}',
+        1 => 'Se muestran {{x}} presentaciones de las {{y}}',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:31
+    'No slideshows' => 'No hay presentaciones',
+
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:33
+    'Show all slideshows' => 'Mostrar todas las presentaciones',
+
+    #: config/common/slideshow.config.php:31
+    'Slides' => 'Diapositivas',
+
+    #. Crud
+    #: config/common/slideshow.config.php:56
+    'Added! This is a nice new slideshow you have here.' => '¡Listo! Menuda nueva presentación que te has hecho.',
+
+    #: config/common/slideshow.config.php:57
+    'The slideshow has been deleted.' => 'Se ha eliminado la presentación.',
+
+    #. General errors
+    #: config/common/slideshow.config.php:60
+    'This slideshow doesn’t exist any more. It has been deleted.' => 'Esta presentación ya no existe. Se ha eliminado.',
+
+    #: config/common/slideshow.config.php:61
+    'We cannot find this slideshow.' => 'No encontramos esta presentación. ¿Estás seguro/a de que existe?',
+
+    #. Deletion popup
+    #: config/common/slideshow.config.php:64
+    'Deleting the slideshow ‘{{title}}’' => 'Eliminar la presentación «{{title}}»',
+
+    #: config/common/slideshow.config.php:68
+    'Yes, delete this slideshow' => array(
+        0 => 'Sí que quiero eliminar esta presentación',
+        1 => 'Sí que quiero eliminar estas {{count}} presentaciones',
+    ),
+
+);
