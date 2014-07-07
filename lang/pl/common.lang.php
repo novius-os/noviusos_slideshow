a639c7d20bab788d4efc069c2293ba6162a327b7
Gilles FELIX <felix@novius.com>
2014-07-07 14:48:18 +0200

    "Add translation files for polish, portuguese and portuguese brazil"

---
+++ lang/pl/common.lang.php
@@ -0,0 +1,145 @@
+<?php
+
+// Generated on 07/07/2014 14:44:21
+
+// 37 out of 40 messages are translated (92%).
+// 148 out of 160 words are translated (92%).
+
+return array(
+    #: config/controller/admin/enhancer.config.php:22
+    'Pick a slideshow:' => '',
+
+    #: config/controller/admin/enhancer.config.php:29
+    'No slideshow selected' => '',
+
+    #: config/controller/admin/enhancer.config.php:30
+    'Pick a slideshow' => '',
+
+    #: config/controller/admin/enhancer.config.php:31
+    'Pick another slideshow' => '',
+
+    #: config/controller/admin/enhancer.config.php:32
+    'Un-select this slideshow' => '',
+
+    #: config/controller/admin/enhancer.config.php:40
+    'Format:' => '',
+
+    #: config/controller/admin/slideshow.config.php:19
+    #: config/common/slideshow.config.php:79
+    'Add a slideshow' => '',
+
+    #: config/controller/admin/slideshow.config.php:45
+    #: config/common/slideshow.config.php:9
+    'Title' => '',
+
+    #: config/controller/admin/slideshow.config.php:68
+    'Image:' => '',
+
+    #: config/controller/admin/slideshow.config.php:76
+    'Image' => '',
+
+    #: config/controller/admin/slideshow.config.php:92
+    'Slide name:' => '',
+
+    #: config/controller/admin/slideshow.config.php:95
+    'Description:' => '',
+
+    #: config/controller/admin/slideshow.config.php:105
+    'Links to:' => '',
+
+    #: config/controller/admin/slideshow.config.php:116
+    'Properties' => '',
+
+    #: config/controller/admin/appdesk.config.php:21
+    'slideshow' => '',
+
+    #: config/controller/admin/appdesk.config.php:22
+    'slideshows' => '',
+
+    #: config/controller/admin/appdesk.config.php:24
+    #: config/common/slideshow.config.php:73
+    '1 slideshow' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:28
+    'Showing 1 slideshow out of {{y}}' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:31
+    'No slideshows' => '',
+
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:33
+    'Show all slideshows' => '',
+
+    #: config/slideshow.config.php:17
+    'Big Flexslider v1.8' => '',
+
+    #: config/slideshow.config.php:37
+    'Small Flexslider v1.8' => '',
+
+    #: config/common/slideshow.config.php:31
+    'Slides' => '',
+
+    #. Crud
+    #: config/common/slideshow.config.php:56
+    'Added! This is a nice new slideshow you have here.' => '',
+
+    #: config/common/slideshow.config.php:57
+    'The slideshow has been deleted.' => '',
+
+    #. General errors
+    #: config/common/slideshow.config.php:60
+    'This slideshow doesn’t exist any more. It has been deleted.' => '',
+
+    #: config/common/slideshow.config.php:61
+    'We cannot find this slideshow.' => '',
+
+    #. Deletion popup
+    #: config/common/slideshow.config.php:64
+    'Deleting the slideshow ‘{{title}}’' => '',
+
+    #: config/common/slideshow.config.php:68
+    'Yes, delete this slideshow' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: views/admin/_form_content.view.php:36
+    'Add a slide' => '',
+
+    #: views/admin/_form_content.view.php:42
+    'Delete' => '',
+
+    #: views/admin/_form_content.view.php:59
+    'Are you sure you want to delete this slide?' => '',
+
+    #: views/admin/warning_not_published.view.php:18
+    'Not published' => '',
+
+    #: views/admin/warning_not_published.view.php:19
+    'To publish this slideshow, add it to a page, a blog post or any other text editor.' => '',
+
+    #: views/admin/warning_not_published.view.php:22
+    '<p>Most Novius OS text editors (also called WYSIWYG editors) feature this button {{preview}} in the toolbar.</p><p>Click it to access the list of applications you can enhance your text with.</p>' => '',
+
+    #: views/admin/warning_not_published.view.php:23
+    'Applications' => '',
+
+    #: views/admin/renderer/page_selector.view.php:6
+    'Add a link' => '',
+
+    #: views/admin/renderer/page_selector.view.php:10
+    'Remove the link' => '',
+
+    #: views/admin/enhancer/preview.view.php:16
+    'Slideshow ‘{{title}}’' => '',
+
+    #: views/admin/enhancer/preview.view.php:17
+    '(format: {{format}})' => '',
+
+);
