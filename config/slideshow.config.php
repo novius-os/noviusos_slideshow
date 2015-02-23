8280196b6adeaea710db579779bf0984e1f38dfe
Martin Smeeckaert <smeeckaert@novius.fr>
2015-02-23 11:38:18 +0100

    Flexslider 2.3

--- config/slideshow.config.php
+++ config/slideshow.config.php
@@ -5,52 +5,53 @@
  * @copyright  2011 Novius
  * @license    GNU Affero General Public License v3 or (at your option) any later version
  *             http://www.gnu.org/licenses/agpl-3.0.html
- * @link http://www.novius-os.org
+ * @link       http://www.novius-os.org
  */
 
 return array(
     'default_format' => 'flexslider-big',
 
-    'formats' => array(
-        'flexslider-big' => array(
-            'view' => 'noviusos_slideshow::flexslider/slideshow',
-            'label' => __('Big Flexslider v1.8'),
+    'formats'        => array(
+        'flexslider-big'   => array(
+            'view'   => 'noviusos_slideshow::flexslider/slideshow',
+            'label'  => __('Flexslider Large v2.3'),
             'config' => array(
                 'slides_with_link' => true,
-                'slides_preview' => true,
-                'width' => '800',
-                'height' => '600',
-                'class' => 'slide-home',
-                'js' => array(
-                    'jquery' => 'static/apps/noviusos_slideshow/js/jquery.min.js',
-                    'flexslider' => 'static/apps/noviusos_slideshow/js/jquery.flexslider.js',
-                    'flexpreview' => 'static/apps/noviusos_slideshow/js/jquery.novius_flexpreview.js',
+                'slides_preview'   => true,
+                'width'            => '800',
+                'height'           => '600',
+                'class'            => 'slide-home',
+                'js'               => array(
+                    'jquery'      => 'static/apps/noviusos_slideshow/js/jquery.min.js',
+                    'flexslider'  => 'static/apps/noviusos_slideshow/js/jquery.flexslider-min.js',
+                    'flexpreview' => 'static/apps/noviusos_slideshow/js/jquery.noviusos_flexpreview.js',
                 ),
-                'css' => array(
-                    'flexslider' => 'static/apps/noviusos_slideshow/css/flexslider.css',
+                'css'              => array(
+                    'flexslider'  => 'static/apps/noviusos_slideshow/css/flexslider.css',
                     'flexpreview' => 'static/apps/noviusos_slideshow/css/flexpreview.css',
                 ),
             ),
         ),
         'flexslider-small' => array(
-            'view' => 'noviusos_slideshow::flexslider/slideshow',
-            'label' => __('Small Flexslider v1.8'),
+            'view'   => 'noviusos_slideshow::flexslider/slideshow',
+            'label'  => __('Flexslider Small v2.3'),
             'config' => array(
                 'slides_with_link' => true,
-                'slides_preview' => true,
-                'width' => '414',
-                'height' => '300',
-                'class' => 'slide-small',
-                'js' => array(
-                    'jquery' => 'static/apps/noviusos_slideshow/js/jquery.min.js',
-                    'flexslider' => 'static/apps/noviusos_slideshow/js/jquery.flexslider.js',
-                    'flexpreview' => 'static/apps/noviusos_slideshow/js/jquery.novius_flexpreview.js',
+                'slides_preview'   => false,
+                'width'            => '414',
+                'height'           => '300',
+                'class'            => 'slide-small',
+                'js'               => array(
+                    'jquery'      => 'static/apps/noviusos_slideshow/js/jquery.min.js',
+                    'flexslider'  => 'static/apps/noviusos_slideshow/js/jquery.flexslider-min.js',
+                    'flexpreview' => 'static/apps/noviusos_slideshow/js/jquery.noviusos_flexpreview.js',
                 ),
-                'css' => array(
-                    'flexslider' => 'static/apps/noviusos_slideshow/css/flexslider.css',
+                'css'              => array(
+                    'flexslider'  => 'static/apps/noviusos_slideshow/css/flexslider.css',
                     'flexpreview' => 'static/apps/noviusos_slideshow/css/flexpreview.css',
                 ),
             ),
-        ),
+        )
+
     ),
 );
