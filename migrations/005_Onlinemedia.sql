d8d61a6064686dc9eac4f36d8d892c8ccf66c7e0
Smith Jeremy <smith@novius.fr>
2016-03-15 12:01:43 +0100

    typo

--- migrations/005_Onlinemedia.sql
+++ migrations/005_Onlinemedia.sql
@@ -1,1 +1,1 @@
-ALTER TABLE  `nos_slideshow_image` ADD `slidimg__onlinemedia_onme_id` INT( 11 ) NULL DEFAULT NULL;
+ALTER TABLE  `nos_slideshow_image` ADD `slidimg_onlinemedia_onme_id` INT( 11 ) NULL DEFAULT NULL;
